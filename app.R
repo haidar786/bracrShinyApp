8e7f09bcc26aa231a0ec94b5d39fa5bfd0ac5e3b
Hussnain Haidar <23493900+haidar786@users.noreply.github.com>
2022-09-30 20:26:21 +0200

    shinny done.

--- app.R
+++ app.R
@@ -8,14 +8,55 @@ ui <- fluidPage(
   titlePanel("advance r lab 5"),
   h1("brac shiny app"),
   sidebarLayout(
-    sidebarPanel(selectInput("functionInput", "function",
-                             choices = c("Server Ping" = pingToServer(),
-                                         "Server time" = checkServerTime()))),
+    sidebarPanel(selectInput("menu", "function",
+                             choices = c("Server Ping",
+                                         "Server Time",
+                                         "Symbol Order Book",
+                                         "Symbol Trades",
+                                         "Symbol Agg Trades",
+                                         "Symbol Average Price",
+                                         "Ticker 24h Price",
+                                         "Ticker Price"))),
     mainPanel(dataTableOutput("table"))
   )
 )
 
 server <- function(input, output) {
-  output$table <- renderDataTable(as.data.frame.AsIs(input$functionInput))
+  values <- reactiveValues()
+
+  observeEvent(input$menu, {
+    switch (input$menu,
+            "Server Ping" = {
+              values$df <- pingToServer()
+            },
+            "Server Time" = {
+              values$df <- checkServerTime()
+            },
+            "Symbol Order Book" = {
+              values$df <- fetchSymbolOrderBook(symbol = "BNBBTC")
+            },
+            "Symbol Trades" = {
+              values$df <- fetchSymbolTrades(symbol = "BNBBTC", limit = 5)
+            },
+            "Symbol Agg Trades" = {
+              values$df <- fetchSymbolAggTrades(symbol = "BNBBTC", limit = 5)
+            },
+            "Symbol Average Price" = {
+              values$df <- fetchSymbolAveragePrice(symbol = "BNBBTC")
+            },
+            "Ticker 24h Price" = {
+              values$df <- fetchTicker24hPrice(symbol = "BNBBTC")
+            },
+            "Ticker Price" = {
+              values$df <- fetchTickerPrice(symbol = "BNBBTC")
+            }
+    )
+  })
+
+  output$table <- renderDataTable(values$df)
 }
 shinyApp(ui = ui, server = server)
+
+
+
+
